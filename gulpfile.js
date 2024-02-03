01d87a5b203b326a1309685c81072a312ea8dedc
Alexandr <kotte48@gmail.com>
2024-02-03 15:09:45 +0300

    Верстка блока с карточками товаров и текстовыми блоками + их адаптив

--- gulpfile.js
+++ gulpfile.js
@@ -91,7 +91,7 @@ export function optimizeRaster () {
           {
             format,
             rename: { suffix: `@${density}x` },
-            width: ({ width }) => Math.ceil(width * density / RAW_DENSITY),
+            // width: ({ width }) => Math.ceil(width * density / RAW_DENSITY),
             jpegOptions: { progressive: true },
           },
         );
